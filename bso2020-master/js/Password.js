615732b06855393c9f7680559ed5db298be829a3
Renjith Jayapalan Nair <renjith@pc-197-118.customer.ask4.lan>
2020-06-15 14:29:06 +0100

    BSOV passwords

--- bso2020-master/js/Password.js
+++ bso2020-master/js/Password.js
@@ -13,41 +13,51 @@ function check(form) { /*function to check userid & password*/
 		/*the following code checkes whether the entered userid and password are matching*/
 		if(form.userid.value == "myuserid" && form.pswrd.value == "mypswrd") {
 				window.open('https://teams.microsoft.com/meetingOptions/?organizerId=a390358c-85fd-4c5f-b56b-eeb084c735a5&tenantId=cf264fc0-aeb8-449f-9054-82ce4454084b&threadId=19_meeting_MDMwYWE1OGUtNzFkYy00NjYzLThmYzgtMzgwZGUyMmJmYTI4@thread.v2&messageId=0&language=en-US')/*opens the target page while Id & password matches*/
+				window.location.reload()
 		}
 		else if(form.userid.value == "myuserid1" && form.pswrd.value == "mypswrd1") {
 				window.open('https://teams.microsoft.com/meetingOptions/?organizerId=a390358c-85fd-4c5f-b56b-eeb084c735a5&tenantId=cf264fc0-aeb8-449f-9054-82ce4454084b&threadId=19_meeting_MDMwYWE1OGUtNzFkYy00NjYzLThmYzgtMzgwZGUyMmJmYTI4@thread.v2&messageId=0&language=en-US')/*opens the target page while Id & password matches*/
+				window.location.reload()
 		}
 
 		else if(form.userid.value == "V.Dimitriou@lboro.ac.uk" && form.pswrd.value == "mypswrd1") {
 				window.open('https://teams.microsoft.com/meetingOptions/?organizerId=a390358c-85fd-4c5f-b56b-eeb084c735a5&tenantId=cf264fc0-aeb8-449f-9054-82ce4454084b&threadId=19_meeting_MDMwYWE1OGUtNzFkYy00NjYzLThmYzgtMzgwZGUyMmJmYTI4@thread.v2&messageId=0&language=en-US')/*opens the target page while Id & password matches*/
+				window.location.reload()
 		}
 
 		else if(form.userid.value == "Malcolm.Cook@lboro.ac.uk" && form.pswrd.value == "mypswrd1") {
 				window.open('https://teams.microsoft.com/meetingOptions/?organizerId=a390358c-85fd-4c5f-b56b-eeb084c735a5&tenantId=cf264fc0-aeb8-449f-9054-82ce4454084b&threadId=19_meeting_MDMwYWE1OGUtNzFkYy00NjYzLThmYzgtMzgwZGUyMmJmYTI4@thread.v2&messageId=0&language=en-US')/*opens the target page while Id & password matches*/
+				window.location.reload()
 		}
 
 		else if(form.userid.value == "J.A.Wright@lboro.ac.uk" && form.pswrd.value == "mypswrd1") {
 				window.open('https://teams.microsoft.com/meetingOptions/?organizerId=a390358c-85fd-4c5f-b56b-eeb084c735a5&tenantId=cf264fc0-aeb8-449f-9054-82ce4454084b&threadId=19_meeting_MDMwYWE1OGUtNzFkYy00NjYzLThmYzgtMzgwZGUyMmJmYTI4@thread.v2&messageId=0&language=en-US')/*opens the target page while Id & password matches*/
+				window.location.reload()
 		}
 
 		else if(form.userid.value == "M.M.Eftekhari@lboro.ac.uk" && form.pswrd.value == "mypswrd1") {
 				window.open('https://teams.microsoft.com/meetingOptions/?organizerId=a390358c-85fd-4c5f-b56b-eeb084c735a5&tenantId=cf264fc0-aeb8-449f-9054-82ce4454084b&threadId=19_meeting_MDMwYWE1OGUtNzFkYy00NjYzLThmYzgtMzgwZGUyMmJmYTI4@thread.v2&messageId=0&language=en-US')/*opens the target page while Id & password matches*/
+				window.location.reload()
 		}
 
 		else if(form.userid.value == "E.Brembilla@lboro.ac.uk" && form.pswrd.value == "mypswrd1") {
 				window.open('https://teams.microsoft.com/meetingOptions/?organizerId=a390358c-85fd-4c5f-b56b-eeb084c735a5&tenantId=cf264fc0-aeb8-449f-9054-82ce4454084b&threadId=19_meeting_MDMwYWE1OGUtNzFkYy00NjYzLThmYzgtMzgwZGUyMmJmYTI4@thread.v2&messageId=0&language=en-US')/*opens the target page while Id & password matches*/
+				window.location.reload()
 		}
 
 		else if(form.userid.value == "A.Oraiopoulos@lboro.ac.uk" && form.pswrd.value == "mypswrd1") {
 				window.open('https://teams.microsoft.com/meetingOptions/?organizerId=a390358c-85fd-4c5f-b56b-eeb084c735a5&tenantId=cf264fc0-aeb8-449f-9054-82ce4454084b&threadId=19_meeting_MDMwYWE1OGUtNzFkYy00NjYzLThmYzgtMzgwZGUyMmJmYTI4@thread.v2&messageId=0&language=en-US')/*opens the target page while Id & password matches*/
+				window.location.reload()
 		}
 
 		else if(form.userid.value == "B.Howard@lboro.ac.uk" && form.pswrd.value == "mypswrd1") {
 				window.open('https://teams.microsoft.com/meetingOptions/?organizerId=a390358c-85fd-4c5f-b56b-eeb084c735a5&tenantId=cf264fc0-aeb8-449f-9054-82ce4454084b&threadId=19_meeting_MDMwYWE1OGUtNzFkYy00NjYzLThmYzgtMzgwZGUyMmJmYTI4@thread.v2&messageId=0&language=en-US')/*opens the target page while Id & password matches*/
+				window.location.reload()
 		}
 
 		else if(form.userid.value == "R.Jayapalan-Nair@lboro.ac.uk" && form.pswrd.value == "mypswrd1") {
 				window.open('https://teams.microsoft.com/meetingOptions/?organizerId=a390358c-85fd-4c5f-b56b-eeb084c735a5&tenantId=cf264fc0-aeb8-449f-9054-82ce4454084b&threadId=19_meeting_MDMwYWE1OGUtNzFkYy00NjYzLThmYzgtMzgwZGUyMmJmYTI4@thread.v2&messageId=0&language=en-US')/*opens the target page while Id & password matches*/
+				window.location.reload()
 		}
 		else {
 				alert("Incorrect Password or Username. \n Username and password are case sensitive.")/*displays error message*/
